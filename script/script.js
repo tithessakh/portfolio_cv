d85d3a21a60e7f71dddc4050c13f01f379f14542
Athanasios Vouduoris <tithessakh@hotmail.com>
2023-06-21 12:56:21 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+/*const tabButtons = document.getElementsByClassName("tabpanel");
+const tabContent = document.getElementsByClassName("tabcontent");
+
+function showPanel(tab){
+    tabContent.forEach(item => {
+        item.style.display='none';
+    });
+    tabContent[tab].style.display="block";
+}*/
+
+const tab = document.getElementsByClassName('tabpanel');
+
+function hide(){
+    tab.style.display="none";
+}
